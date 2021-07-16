6d38dd15f1d7c559992fe99af4a32ca7d05047e2
RJ <rjkaz@comcast.net>
2021-07-15 23:51:05 -0400

    images

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
-  console.log("App executed");
-  
-  
 
-
+  const handlePress = () => console.log("Text Pressed");
+ 
   return (
-    <View style={styles.container}>
-      <Text> Hello React Native </Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+      Hello React Native - This a susseouhfiwfiuwehbfiuweiufhweiufbwiebfhiwbfiuwebfiwebiwebifbweifbweifbwibfiwebfiwbfiwbfibweifbwibfiwbfiwbfiwbefibwefibweifbwei </Text>
+      
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
+    
   },
 });
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # react-native-store
 Creating a small online store with React Native 
-left off at 34:35 - debuging in chrome
+left off at 53:51 - images
