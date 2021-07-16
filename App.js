4065bc628e0eba872db7426893a52bba7fbd0aeb
RJ <rjkaz@comcast.net>
2021-07-16 19:02:16 -0400

    layouts

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TouchableNativeFeedback, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Alert, Text, TouchableNativeFeedback, View, Image, SafeAreaView, Button, Platform, StatusBar } from 'react-native';
 
 export default function App() {
 
  
   return (
-    <SafeAreaView style={styles.container}>
-      
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <Button 
+      title="Click Me" 
+      onPress={() => console.log("Button tapped")}
+        // onPress={() => 
+        //  Alert.prompt("My title", "My message", (text) => console.log(text) )}
+        // color="orange"
+        // title="Click Me" 
+        // onPress={() => console.log("Button tapped")}
+        // onPress={() => alert('Button Tapped')}
+        // onPress={() => Alert.alert("My title", "My message", [
+        //  {text: "Yes", onPress: () => console.log("Yes")},
+        //  {text: "No", onPress: () => console.log("No") },
+        // ])}
+        />
       
     </SafeAreaView>
   );
 }
 
+const containerStyle = { backgroundColor: "orange"};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    // paddingTop: Platform.OS === 'android' ? 20 : 0,
+    // justifyContent: 'center',
+    // alignItems: 'center',
     
   },
 });
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # react-native-store
 Creating a small online store with React Native 
-left off at 1:04:30 - button
+left off at 1:18:06 - button
